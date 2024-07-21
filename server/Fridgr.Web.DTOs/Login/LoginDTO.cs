613596fdfd382eb22e4aa944a94b2096fed35393
Hris <xrisko06@abv.bg>
2024-07-21 16:59:03 +0300

    Add Login endpoint.

---
+++ server/Fridgr.Web.DTOs/Login/LoginDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Fridgr.Web.DTOs.Login
+{
+    public class LoginDTO
+    {
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+    }
+}
--- server/webapi/Controllers/AuthenticationController.cs
+++ server/webapi/Controllers/AuthenticationController.cs
@@ -2,9 +2,11 @@
 using Fridgr.Services.Data.Images;
 using Fridgr.Services.Data.Tokens;
 using Fridgr.Services.Mapping;
+using Fridgr.Web.DTOs.Login;
 using Fridgr.Web.DTOs.Register;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace webapi.Controllers
 {
@@ -53,6 +55,38 @@ namespace webapi.Controllers
 
             return StatusCode(StatusCodes.Status500InternalServerError,
                 $"Failed to create user: {string.Join(" ", result.Errors.Select(e => e.Description))}");
+        }
+
+        [HttpPost("login")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(LoginResponse))]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Login([FromBody] LoginDTO model)
+        {
+            _logger.LogInformation("Login called");
+
+            var user = await _userManager.FindByNameAsync(model.Username);
+
+            if (user == null || !await _userManager.CheckPasswordAsync(user, model.Password))
+                return Unauthorized();
+
+            JwtSecurityToken token = _tokenService.GenerateJwt(model.Username);
+
+            var refreshToken = _tokenService.GenerateRefreshToken();
+
+            user.RefreshToken = refreshToken;
+            user.RefreshTokenExpiry = DateTime.UtcNow.AddHours(6);
+
+            await _userManager.UpdateAsync(user);
+
+            _logger.LogInformation("Login succeeded");
+
+            return Ok(new LoginResponse
+            {
+                JwtToken = new JwtSecurityTokenHandler().WriteToken(token),
+                Expiration = token.ValidTo,
+                RefreshToken = refreshToken
+            });
         }
     }
 }
