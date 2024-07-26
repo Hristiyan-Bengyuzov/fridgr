0289b59152edf0aaaf7e212fcdf5363267811cb9
Hris <xrisko06@abv.bg>
2024-07-26 13:07:37 +0300

    Refactor RecipeCard.

--- client/src/components/recipes/RecipeCard.tsx
+++ client/src/components/recipes/RecipeCard.tsx
@@ -1,18 +1,28 @@
-import { Card } from "antd";
+import { Card, Button } from "antd";
 import { RecipeDTO } from "../../types/recipes/recipeDTOs";
+import { useNavigate } from "react-router-dom";
 
 interface RecipeCardProps {
   recipe: RecipeDTO;
 }
 
 const RecipeCard: React.FC<RecipeCardProps> = ({ recipe }) => {
+  const navigate = useNavigate();
+
+  const goToRecipe = () => {
+    navigate(`/recipe/${recipe.id}`);
+  };
+
   return (
     <Card
       hoverable
       style={{ width: 240 }}
-      cover={<img alt={recipe.name} src={recipe.image} />}
+      cover={<img alt={recipe.name} src={recipe.image} height={'175px'} />}
     >
       <Card.Meta title={recipe.name} />
+      <Button type="primary" onClick={goToRecipe} style={{ marginTop: 16 }}>
+        View Recipe
+      </Button>
     </Card>
   );
 };
