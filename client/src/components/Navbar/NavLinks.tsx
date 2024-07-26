2ab9a129e4712da845cc19ab5ce04572a58d1c01
Hris <xrisko06@abv.bg>
2024-07-27 00:14:19 +0300

    Refactor NavLinks.

--- client/src/components/Navbar/NavLinks.tsx
+++ client/src/components/Navbar/NavLinks.tsx
@@ -1,7 +1,12 @@
 import { Link } from "react-router-dom";
 import NavTogglerProps from "../../types/Navbar/NavbarProps";
+import { useContext } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
+import { Avatar } from "antd";
 
 export default function NavLinks({ expanded, setExpanded }: NavTogglerProps) {
+  const authContext = useContext(AuthContext);
+
   return (
     <div
       className={`collapse navbar-collapse ${expanded ? "show" : ""}`}
@@ -15,15 +20,6 @@ export default function NavLinks({ expanded, setExpanded }: NavTogglerProps) {
         </li>
         <li className="nav-item">
           <Link
-            to="/about"
-            className="nav-link"
-            onClick={() => setExpanded(false)}
-          >
-            About
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link
             to="/recipes"
             className="nav-link"
             onClick={() => setExpanded(false)}
@@ -31,15 +27,56 @@ export default function NavLinks({ expanded, setExpanded }: NavTogglerProps) {
             Recipes
           </Link>
         </li>
-        <li className="nav-item">
-          <Link
-            to="/login"
-            className="nav-link"
-            onClick={() => setExpanded(false)}
-          >
-            Login
-          </Link>
-        </li>
+        {authContext?.user ? (
+          <>
+            <li className="nav-item">
+              <Link
+                to="/profile"
+                className="nav-link"
+                onClick={() => setExpanded(false)}
+              >
+                <div style={{ display: "flex" }}>
+                  Profile
+                  <Avatar
+                    src={authContext.user.image}
+                    size={36}
+                    style={{ marginLeft: "8px" }}
+                  />
+                </div>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/logout"
+                className="nav-link"
+                onClick={() => setExpanded(false)}
+              >
+                Logout
+              </Link>
+            </li>
+          </>
+        ) : (
+          <>
+            <li className="nav-item">
+              <Link
+                to="/register"
+                className="nav-link"
+                onClick={() => setExpanded(false)}
+              >
+                Register
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/login"
+                className="nav-link"
+                onClick={() => setExpanded(false)}
+              >
+                Login
+              </Link>
+            </li>
+          </>
+        )}
       </ul>
     </div>
   );
